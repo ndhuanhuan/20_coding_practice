83c9ff8c0eb854649bac6140d3fa2f648c3a7549
ndhuanhuan <ndhuanhuan@gmail.com>
2020-09-14 23:02:38 -0500

    Create 1585. _Check If String Is Transformable With Substring Sort Operations.cpp

---
+++ 1401-1600/1585. _Check If String Is Transformable With Substring Sort Operations.cpp
@@ -0,0 +1,18 @@
+// https://zxi.mytechroad.com/blog/string/leetcode-1585-check-if-string-is-transformable-with-substring-sort-operations/
+class Solution {
+public:
+  bool isTransformable(string s, string t) {
+    vector<deque<int>> idx(10);
+    for (int i = 0; i < s.length(); ++i)
+      idx[s[i] - '0'].push_back(i);
+    for (char c : t) {
+      const int d  = c - '0';
+      if (idx[d].empty()) return false;
+      for (int i = 0; i < d; ++i)
+        if (!idx[i].empty() && idx[i].front() < idx[d].front())
+          return false;
+      idx[d].pop_front();      
+    }
+    return true;
+  }
+};
