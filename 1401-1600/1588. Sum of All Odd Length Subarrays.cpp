fbdead59426aaf88171658a3bf22bc427057b628
ndhuanhuan <ndhuanhuan@gmail.com>
2020-09-21 18:01:28 -0700

    Create 1588. Sum of All Odd Length Subarrays.cpp

---
+++ 1401-1600/1588. Sum of All Odd Length Subarrays.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/sum-of-all-odd-length-subarrays/discuss/854184/JavaC%2B%2BPython-O(N)-Time-O(1)-Space
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& A) {
+        int res = 0, n = A.size();
+        for (int i = 0; i < n; ++i) {
+            res += ((i + 1) * (n - i) + 1) / 2 * A[i];
+        }
+        return res;
+    }
+};
