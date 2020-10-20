23e0c02881448b15ab82d018b8313e5f61ce3e6f
ndhuanhuan <ndhuanhuan@gmail.com>
2020-10-19 18:14:14 -0700

    Create 1624. Largest Substring Between Two Equal Characters.cpp

---
+++ 1601-1800/1624. Largest Substring Between Two Equal Characters.cpp
@@ -0,0 +1,13 @@
+
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        unordered_map<char, int> mp; // key: character, value: least index of character in s
+        int len = -1;
+        for(int i = 0; i < s.length(); i++) {
+            if(mp.count(s[i])) len = max(len, i - mp[s[i]] - 1);
+            else mp[s[i]] = i;
+        }
+        return len;
+    }
+};
