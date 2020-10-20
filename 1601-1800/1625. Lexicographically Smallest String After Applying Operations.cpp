042c973ef49269f63618a7a54b80e39d29931e28
ndhuanhuan <ndhuanhuan@gmail.com>
2020-10-19 18:31:31 -0700

    Create 1625. Lexicographically Smallest String After Applying Operations.cpp

---
+++ 1601-1800/1625. Lexicographically Smallest String After Applying Operations.cpp
@@ -0,0 +1,19 @@
+// https://zxi.mytechroad.com/blog/searching/leetcode-1625-lexicographically-smallest-string-after-applying-operations/
+class Solution {
+public:
+  string findLexSmallestString(string s, int a, int b) {
+    unordered_set<string> seen;    
+    string ans(s);
+    function<void(string)> dfs = [&](const string& s) {
+      if (!seen.insert(s).second) return;
+      ans = min(ans, s);
+      string t(s);
+      for (int i = 1; i < s.length(); i += 2)
+        t[i] = (t[i] - '0' + a) % 10 + '0';
+      dfs(t);
+      dfs(s.substr(b) + s.substr(0, b));
+    };    
+    dfs(s);
+    return ans;
+  }
+};
