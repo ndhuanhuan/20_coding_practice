00a51580d054961c16573114d7a4696afe7ea10a
ndhuanhuan <ndhuanhuan@gmail.com>
2020-10-19 19:05:18 -0700

    Create 1626. Best Team With No Conflicts.cpp

---
+++ 1601-1800/1626. Best Team With No Conflicts.cpp
@@ -0,0 +1,20 @@
+// https://zxi.mytechroad.com/blog/dynamic-programming/leetcode-1626-best-team-with-no-conflicts/
+class Solution {
+public:
+  int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+    const int n = scores.size();
+    vector<pair<int, int>> players(n);
+    for (int i = 0; i < n; ++i) 
+      players[i] = {ages[i], scores[i]};
+    sort(rbegin(players), rend(players));
+    // dp[i] = max score of the first i players, i must be selected.
+    vector<int> dp(n);
+    for (int i = 0; i < n; ++i) {      
+      for (int j = 0; j < i; ++j)
+        if (players[i].second <= players[j].second)
+          dp[i] = max(dp[i], dp[j]);
+      dp[i] += players[i].second;
+    }
+    return *max_element(begin(dp), end(dp));
+  }
+};
