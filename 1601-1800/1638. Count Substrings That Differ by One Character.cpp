251308d09e7f87a58bf6661060363e89609f0f9b
ndhuanhuan <ndhuanhuan@gmail.com>
2020-11-03 18:19:13 -0800

    Create 1638. Count Substrings That Differ by One Character.cpp

---
+++ 1601-1800/1638. Count Substrings That Differ by One Character.cpp
@@ -0,0 +1,14 @@
+// https://zxi.mytechroad.com/blog/string/leetcode-1638-count-substrings-that-differ-by-one-character/
+class Solution {
+public:
+  int countSubstrings(string s, string t) {
+    const int m = s.length();
+    const int n = t.length();
+    int ans = 0, diff = 0;
+    for (int i = 0; i < m; ++i)
+      for (int j = 0; j < n; ++j, diff = 0)
+        for (int p = 0; i + p < m && j + p < n && diff <= 1; ++p)          
+          if ((diff += (s[i + p] != t[j + p])) == 1) ++ans;
+    return ans;
+  }
+};
