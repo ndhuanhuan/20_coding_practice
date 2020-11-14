fabbf41b5e45578d92789f9b9f6b563d8d103b02
ndhuanhuan <ndhuanhuan@gmail.com>
2020-11-14 17:32:31 -0600

    Create 1652. Defuse the Bomb.cpp

---
+++ 1601-1800/1652. Defuse the Bomb.cpp
@@ -0,0 +1,13 @@
+// https://zxi.mytechroad.com/blog/algorithms/array/leetcode-1652-defuse-the-bomb/
+class Solution {
+public:
+  vector<int> decrypt(vector<int>& code, int k) {    
+    const int n = code.size();
+    vector<int> ans(n);
+    int sign = k > 0 ? 1 : -1;
+    for (int i = 0; i < n; ++i)
+      for (int j = 1; j <= abs(k); ++j)
+        ans[i] += code[(i + j * sign + n) % n];
+    return ans;
+  }
+};
