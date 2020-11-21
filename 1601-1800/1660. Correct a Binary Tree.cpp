3b8e4f33635679a88fd25bd5872a45f2bdfb4420
ndhuanhuan <ndhuanhuan@gmail.com>
2020-11-20 21:33:39 -0800

    Create 1660. Correct a Binary Tree.cpp

---
+++ 1601-1800/1660. Correct a Binary Tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* correctBinaryTree(TreeNode* root) {
+        if (root == NULL){
+            return NULL;
+        }
+        if (root->right != NULL && s.find(root->right) != s.end()){
+            return NULL;
+        }
+        
+        root->right = correctBinaryTree(root->right);
+        root->left = correctBinaryTree(root->left);
+        
+        s.insert(root);
+        
+        return root;
+    }
+
+private:
+    unordered_set<TreeNode*> s;
+};
