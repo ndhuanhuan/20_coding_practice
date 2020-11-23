e603686e6e62323bd0114b868fca737dfecb2e4e
ndhuanhuan <ndhuanhuan@gmail.com>
2020-11-23 11:12:50 -0800

    Update 1665. Minimum Initial Energy to Finish Tasks.cpp

--- 1601-1800/1665. Minimum Initial Energy to Finish Tasks.cpp
+++ 1601-1800/1665. Minimum Initial Energy to Finish Tasks.cpp
@@ -5,10 +5,10 @@ public:
     int minimumEffort(vector<vector<int>>& A) {
         int res = 0;
         for (auto &a : A)
-            a[0] = a[1] - a[0];
+            a[0] = a[1] - a[0]; // a[0] now is prev_saved
         sort(A.begin(), A.end());
         for (auto &a : A)
-            res = max(res + a[1] - a[0], a[1]);
+            res = max(res + a[1] - a[0], a[1]); // To start a new task, we need to add mmin - prev_saved energy. 
         return res;
     }
 };
