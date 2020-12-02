aa3d9da7960d9e0624756f4d865b2028019c5485
ndhuanhuan <ndhuanhuan@gmail.com>
2020-12-01 21:02:52 -0800

    Create 1674. __Minimum Moves to Make Array Complementary.cpp

---
+++ 1601-1800/1674. __Minimum Moves to Make Array Complementary.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/minimum-moves-to-make-array-complementary/discuss/953078/C%2B%2B-Prefix-sum-O(n-%2B-limit)-with-detailed-examples
+class Solution {
+public:
+    int minMoves(vector<int>& nums, int limit) {
+        int N = nums.size();
+        vector<int> memo(limit*2 + 2, 0);
+        for (int i = 0; i < N/2; ++i) {
+            int l = nums[i], r = nums[N-1-i];
+            --memo[min(l, r) + 1];
+            --memo[l + r];
+            ++memo[l + r + 1];
+            ++memo[max(l, r) + limit + 1];
+        }
+        int ans = N, curr = N;
+        for (int i = 2; i <= limit*2; ++i) {
+            curr += memo[i];
+            ans = min(ans, curr);
+        }
+        return ans;
+    }
+};
