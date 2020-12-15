ef165810fc573d0dada7d40b6fb99e40b651b258
ndhuanhuan <ndhuanhuan@gmail.com>
2020-12-14 20:06:26 -0800

    Create 1690. Stone Game VII.cpp

---
+++ 1601-1800/1690. Stone Game VII.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/stone-game-vii/discuss/970262/C%2B%2B-Bottom-up-DP-O(N2)-time
+class Solution {
+public:
+    int stoneGameVII(vector<int>& A) {
+        int N = A.size();
+        vector<int> sum(N + 1);
+        for (int i = 0; i < N; ++i) sum[i + 1] = sum[i] + A[i];
+        vector<vector<int>> dp(N, vector<int>(N));
+        for (int len = 2; len <= N; ++len) {
+            for (int i = 0; i <= N - len; ++i) {
+                int j = i + len - 1;
+                dp[i][j] = max(sum[j + 1] - sum[i + 1] - dp[i + 1][j], sum[j] - sum[i] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][N - 1];
+    }
+};
