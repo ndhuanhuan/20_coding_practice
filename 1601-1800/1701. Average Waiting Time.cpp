8a5131edfd95f33667b209b1effecee396c27e28
ndhuanhuan <ndhuanhuan@gmail.com>
2020-12-31 15:07:22 -0800

    Create 1701. Average Waiting Time.cpp

---
+++ 1601-1800/1701. Average Waiting Time.cpp
@@ -0,0 +1,14 @@
+// https://zxi.mytechroad.com/blog/simulation/leetcode-1701-average-waiting-time/
+class Solution {
+public:
+  double averageWaitingTime(vector<vector<int>>& customers) {
+    int t = 0;
+    double w = 0;
+    for (const auto& c : customers) {
+      if (c[0] > t) t = c[0];
+      t += c[1];
+      w += t - c[0];
+    }
+    return w / customers.size();
+  }
+};
