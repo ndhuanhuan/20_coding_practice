9826e2f35703ac93c5b12157d152a22f61c0ff6b
ndhuanhuan <ndhuanhuan@gmail.com>
2021-01-04 22:22:47 -0800

    Create 1713. Minimum Operations to Make a Subsequence.cpp

---
+++ 1601-1800/1713. Minimum Operations to Make a Subsequence.cpp
@@ -0,0 +1,21 @@
+// https://zxi.mytechroad.com/blog/dynamic-programming/leetcode-1713-minimum-operations-to-make-a-subsequence/
+class Solution {
+public:
+  int minOperations(vector<int>& target, vector<int>& arr) {
+    unordered_map<int, int> m;
+    for (int i = 0; i < target.size(); ++i)
+      m[target[i]] = i;
+    vector<int> s;
+    for (int a : arr) {
+      auto mit = m.find(a);
+      if (mit == end(m)) continue;
+      const int idx = mit->second;
+      auto it = lower_bound(begin(s), end(s), idx);
+      if (it == end(s))
+        s.push_back(idx);
+      else
+        *it = idx;
+    }
+    return target.size() - s.size();
+  }
+};
