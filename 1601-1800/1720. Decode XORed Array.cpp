d985b8b4a14d868a19b7f0dd307197973cf5b748
ndhuanhuan <ndhuanhuan@gmail.com>
2021-01-11 17:28:37 -0800

    Create 1720. Decode XORed Array.cpp

---
+++ 1601-1800/1720. Decode XORed Array.cpp
@@ -0,0 +1,11 @@
+// https://zxi.mytechroad.com/blog/bit/leetcode-1720-decode-xored-array/
+class Solution {
+public:
+  vector<int> decode(vector<int>& encoded, int first) {
+    const int n = encoded.size() + 1;
+    vector<int> ans(n, first);
+    for (int i = 0; i + 1 < n; ++i)
+      ans[i + 1] = ans[i] ^ encoded[i];
+    return ans;
+  }
+};
