265c18a8c05a940c068908f5a6bef3366b22678d
ndhuanhuan <ndhuanhuan@gmail.com>
2021-02-15 11:39:29 -0600

    Create 1758. Minimum Changes To Make Alternating Binary String.cpp

---
+++ 1601-1800/1758. Minimum Changes To Make Alternating Binary String.cpp
@@ -0,0 +1,12 @@
+// https://zxi.mytechroad.com/blog/string/leetcode-1758-minimum-changes-to-make-alternating-binary-string/
+class Solution {
+public:
+  int minOperations(string s) {
+    int c1 = 0, c2 = 0;
+    for (int i = 0; i < s.length(); ++i) {
+      c1 += (s[i] - '0' == i % 2);
+      c2 += (s[i] - '0' != i % 2);
+    }
+    return min(c1, c2);
+  }
+};
