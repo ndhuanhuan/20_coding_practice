0c820d4ac83441c28d005ab4dfa3d5e0fc862ccd
ndhuanhuan <ndhuanhuan@gmail.com>
2021-02-15 12:10:11 -0600

    Create 1760. Minimum Limit of Balls in a Bag.cpp

---
+++ 1601-1800/1760. Minimum Limit of Balls in a Bag.cpp
@@ -0,0 +1,18 @@
+// https://zxi.mytechroad.com/blog/algorithms/binary-search/leetcode-1760-minimum-limit-of-balls-in-a-bag/
+class Solution {
+public:
+  int minimumSize(vector<int>& nums, int maxOperations) {
+    int l = 1, r = *max_element(begin(nums), end(nums));
+    while (l < r) {
+      const int m = l + (r - l) / 2;
+      int count = 0;
+      for (int x : nums) 
+        count += (x - 1) / m;
+      if (count <= maxOperations)
+        r = m;
+      else
+        l = m + 1;
+    }
+    return l;
+  }
+};
