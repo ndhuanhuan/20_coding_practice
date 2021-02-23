2eab7c05911221923a3b16a6e898841a505029a0
ndhuanhuan <ndhuanhuan@gmail.com>
2021-02-22 18:37:06 -0800

    Create 1769. Minimum Number of Operations to Move All Balls to Each Box.cpp

---
+++ 1601-1800/1769. Minimum Number of Operations to Move All Balls to Each Box.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/discuss/1075474/C%2B%2BJava-O(n)-LTR-%2B-RTL
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        vector<int> res(boxes.length()); 
+        for (int i = 0, ops = 0, cnt = 0; i < boxes.length(); ++i) {
+           res[i] += ops;
+           cnt += boxes[i] == '1' ? 1 : 0;
+           ops += cnt;
+        }
+        for (int i = boxes.length() - 1, ops = 0, cnt = 0; i >= 0; --i) {
+            res[i] += ops;
+            cnt += boxes[i] == '1' ? 1 : 0;
+            ops += cnt;
+        }
+        return res;
+    }
+};
