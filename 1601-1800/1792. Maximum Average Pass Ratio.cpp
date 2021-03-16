d56a61415f774d099d41e0b404223aa4016ea7ec
ndhuanhuan <ndhuanhuan@gmail.com>
2021-03-15 18:21:26 -0700

    Create 1792. Maximum Average Pass Ratio.cpp

---
+++ 1601-1800/1792. Maximum Average Pass Ratio.cpp
@@ -0,0 +1,21 @@
+// https://zxi.mytechroad.com/blog/priority-queue/leetcode-1792-maximum-average-pass-ratio/
+class Solution {
+public:
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        auto profit = [&](double pass, double total) {
+            return (pass + 1) / (total + 1) - pass / total;
+        };
+        double total = 0;
+        priority_queue<pair<double, array<int, 2>>> pq;
+        for (auto &c : classes) {
+            total += (double) c[0] / c[1];
+            pq.push({profit(c[0], c[1]), {c[0], c[1]}});
+        }
+        while (extraStudents--) {
+            auto [added_profit, c] = pq.top(); pq.pop();
+            total += added_profit;
+            pq.push({profit(c[0] + 1, c[1] + 1), {c[0] + 1, c[1] + 1}});
+        }
+        return total / classes.size();
+    }
+};
