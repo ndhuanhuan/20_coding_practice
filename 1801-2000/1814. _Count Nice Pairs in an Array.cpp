a666067fa1ff1695a5e5335557ed6a6dd820402c
ndhuanhuan <ndhuanhuan@gmail.com>
2021-04-03 20:49:04 -0500

    Create 1814. _Count Nice Pairs in an Array.cpp

---
+++ 1801-2000/1814. _Count Nice Pairs in an Array.cpp
@@ -0,0 +1,15 @@
+// https://zxi.mytechroad.com/blog/hashtable/leetcode-1814-count-nice-pairs-in-an-array/
+class Solution {
+public:
+  int countNicePairs(vector<int>& nums) {
+    constexpr int kMod = 1e9 + 7;
+    unordered_map<int, int> m;
+    long ans = 0;
+    for (int x : nums) {      
+      string s = to_string(x);
+      reverse(begin(s), end(s));
+      ans += m[x - stoi(s)]++;
+    }
+    return ans % kMod;
+  }
+};
