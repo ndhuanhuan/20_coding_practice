bfb76da7c5b95164df4db6e509fd452f7ba409d0
ndhuanhuan <ndhuanhuan@gmail.com>
2021-04-26 18:30:51 -0700

    Create 1838. _Frequency of the Most Frequent Element.cpp

---
+++ 1801-2000/1838. _Frequency of the Most Frequent Element.cpp
@@ -0,0 +1,19 @@
+// https://zxi.mytechroad.com/blog/sliding-window/leetcode-1838-frequency-of-the-most-frequent-element/
+
+class Solution {
+public:
+  int maxFrequency(vector<int>& nums, int k) {
+    sort(begin(nums), end(nums));
+    int l = 0;
+    long sum = 0;
+    int ans = 0;
+    for (int r = 0; r < nums.size(); ++r) {
+      sum += nums[r];      
+      while (l < r && 
+             sum + k < static_cast<long>(nums[r]) * (r - l + 1))
+        sum -= nums[l++];
+      ans = max(ans, r - l + 1);
+    }
+    return ans;
+  }
+};
