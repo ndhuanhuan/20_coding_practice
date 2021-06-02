02a8bbd6b7b92b5cabdcf373d759a38493c66afc
ndhuanhuan <ndhuanhuan@gmail.com>
2021-06-01 17:43:41 -0700

    Create 1876. Substrings of Size Three with Distinct Characters.cpp

---
+++ 1801-2000/1876. Substrings of Size Three with Distinct Characters.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+        int cnt=0,n=s.size();
+        unordered_map<char,int> map;
+        for(int i=0;i<n;i++){
+            if(i>2 and --map[s[i-3]] == 0) map.erase(s[i-3]);
+            map[s[i]]++;
+            if(map.size() == 3) cnt++;
+        }
+        return cnt;
+    }
+};
