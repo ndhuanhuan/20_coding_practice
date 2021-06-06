a1a7a32ecc68f9ea61ae568ee3da6194930ec248
ndhuanhuan <ndhuanhuan@gmail.com>
2021-06-06 11:45:27 -0700

    Create 1889. Minimum Space Wasted From Packaging.cpp

---
+++ 1801-2000/1889. Minimum Space Wasted From Packaging.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/minimum-space-wasted-from-packaging/discuss/1254116/JavaC%2B%2BPython-Binary-Search-should-not-use-prefix-sum
+class Solution {
+public:
+    int minWastedSpace(vector<int>& A, vector<vector<int>>& boxes) {
+        sort(A.begin(), A.end());
+        long res = LONG_MAX, mod = 1e9 + 7, sumA = 0;
+        for (int a : A)
+            sumA += a;
+        for (auto& B : boxes) {
+            sort(B.begin(), B.end());
+            if (B[B.size() - 1] < A[A.size() - 1]) continue;
+            long cur = 0, i = 0, j;
+            for (int b : B) {
+                j = upper_bound(A.begin(), A.end(), b) - A.begin();
+                cur += b * (j - i);
+                i = j;
+            }
+            res = min(res, cur);
+        }
+        return res < LONG_MAX ? (res - sumA) % mod : -1;
+    }
+};
