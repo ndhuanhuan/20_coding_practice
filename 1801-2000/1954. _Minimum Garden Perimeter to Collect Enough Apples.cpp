45b3f75832554710997a0149b4cf35df345d3c33
ndhuanhuan <ndhuanhuan@gmail.com>
2021-08-01 17:35:47 -0700

    Update 1954. _Minimum Garden Perimeter to Collect Enough Apples.cpp

--- 1801-2000/1954. _Minimum Garden Perimeter to Collect Enough Apples.cpp
+++ 1801-2000/1954. _Minimum Garden Perimeter to Collect Enough Apples.cpp
@@ -24,3 +24,25 @@ public:
         return 8 * x;
     }
 };
+
+// https://baike.baidu.com/item/%E5%B9%B3%E6%96%B9%E5%92%8C%E5%85%AC%E5%BC%8F/3264126
+// Use Approach No 1's conclusion, when distance = n, total is 3n*n. 
+// We need to calculate "distance=1" + "distance=2" + ... + "distance=n"
+// We can convert it to 12*(1^2 + 2^2 + ... +  n^2). => don't forget each square have 4 edges.
+// since sum of squares of consecutive numbers is (2n^3 + 3n^2 + n) / 6
+// combine together it is 4n^3 + 6n^2 + 2n
+// Then binary search
+class Solution {
+public:
+    long long minimumPerimeter(long long x) {
+        long long l = 1,  r = 100000, m;
+        while (l < r) {
+            m = (l + r) / 2;
+            if (m * m * m * 4 + m * m * 6 + m * 2 >= x)
+                r = m;
+            else
+                l = m + 1;
+        }
+        return l * 8;
+    }
+};
