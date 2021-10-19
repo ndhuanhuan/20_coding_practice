87dbd5eb9b0af929abf59ccfed121ffae7bf8bc7
ndhuanhuan <ndhuanhuan@gmail.com>
2021-10-18 17:48:26 -0700

    Create 2038. Remove Colored Pieces if Both Neighbors are the Same Color.cpp

---
+++ 2001-2200/2038. Remove Colored Pieces if Both Neighbors are the Same Color.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color/submissions/
+class Solution {
+public:
+    bool winnerOfGame(string s) {
+        int sum[2] = {};
+        for (int i = 0, N = s.size(); i < N;) {
+            int c = s[i], cnt = 0;
+            while (i < N && c == s[i]) ++i, ++cnt;
+            sum[c - 'A'] += max(0, cnt - 2);
+        }
+        return sum[0] > sum[1];
+    }
+};
