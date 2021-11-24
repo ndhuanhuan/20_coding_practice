8df6b0897c3b718efd4c46a21f4a37a89e5ba1fe
ndhuanhuan <ndhuanhuan@gmail.com>
2021-11-23 18:20:52 -0800

    Create 2070. Most Beautiful Item for Each Query.cpp

---
+++ 2001-2200/2070. Most Beautiful Item for Each Query.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> maximumBeauty(vector<vector<int>>& A, vector<int>& Q) {
+        vector<pair<int, int>> queries;
+        for (int i = 0; i < Q.size(); ++i) queries.push_back({Q[i],i});
+        sort(begin(queries), end(queries));
+        sort(begin(A), end(A));
+        int i = 0, N = A.size(), maxBeauty = 0;
+        vector<int> ans(Q.size());
+        for (auto q : queries) {
+            auto &[query, index] = q;
+            for (; i < N && A[i][0] <= query; ++i) maxBeauty = max(maxBeauty, A[i][1]);
+            ans[index] = maxBeauty;
+        }
+        return ans;
+    }
+};
