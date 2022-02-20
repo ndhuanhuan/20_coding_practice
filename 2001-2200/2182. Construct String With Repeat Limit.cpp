8b6eaa6a8b42e4334b42c2f6d1eac2a30a00cbc4
ndhuanhuan <ndhuanhuan@gmail.com>
2022-02-19 21:10:02 -0800

    Create 2182. Construct String With Repeat Limit.cpp

---
+++ 2001-2200/2182. Construct String With Repeat Limit.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string repeatLimitedString(string s, int repeatLimit) {
+        vector<int> cnt(26, 0);
+        for(auto c: s) {
+            cnt[c-'a']++;
+        }
+        string res;
+        while(true) {
+            int i = 25;
+            bool onlyOne = false;
+            for(; i >= 0; --i) {
+                if(res.size() && res.back()-'a' == i && cnt[i]) {
+                    onlyOne = true;
+                    continue;
+                }
+                if(cnt[i]) break; // this can be onlyOne or repeat limit one, it depends 
+            }
+            // cout << i << endl;
+            if(i == -1) break;
+            int fill = onlyOne ? 1 : min(cnt[i], repeatLimit);
+            cnt[i] -= fill;
+            while (fill--) res += 'a' + i;
+        }
+        
+        return res;
+    }
+};
