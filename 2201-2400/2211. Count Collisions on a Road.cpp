7a7891aef40c040d98ec336d41acc4d5723775c8
ndhuanhuan <ndhuanhuan@gmail.com>
2022-03-20 12:21:43 -0700

    Create 2211. Count Collisions on a Road.cpp

---
+++ 2201-2400/2211. Count Collisions on a Road.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/count-collisions-on-a-road/discuss/1865557/Not-a-stupid-problem!
+class Solution {
+public:
+    int countCollisions(string dir) {
+        
+        int res(0), n(size(dir)), i(0), carsFromRight(0);
+        
+        while (i < n and dir[i] == 'L') i++; // skipping all the cars going to left from beginning as they will never collide
+        
+        for ( ; i<n; i++) {
+            if (dir[i] == 'R')  carsFromRight++;
+            else {
+                // if dir[i] == 'S' then there will be carsFromRight number of collission
+                // if dir[i] == 'L' then there will be carsFromRight+1 number of collision (one collision for the rightmost cars and carsFromRight collision for the cars coming from left)
+                res += (dir[i] == 'S') ? carsFromRight : carsFromRight+1;
+                carsFromRight = 0;
+            }
+        }
+        return res;
+    }
+};
