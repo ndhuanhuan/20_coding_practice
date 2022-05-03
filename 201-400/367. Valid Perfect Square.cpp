c63a983b829af252167ca155b21ca73c6b39a961
ndhuanhuan <ndhuanhuan@gmail.com>
2022-05-03 11:04:13 -0700

    Update 367. Valid Perfect Square.cpp

--- 201-400/367. Valid Perfect Square.cpp
+++ 201-400/367. Valid Perfect Square.cpp
@@ -1,4 +1,25 @@
-https://www.hrwhisper.me/leetcode-valid-perfect-square/
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long l = (long)1;
+        long r = (long)num + 1;
+        
+        while(l < r) {
+            long mid = l + (r - l) / 2;
+            long square = mid * mid;
+            if (square == num) return true;
+            else if(square > num) r = mid;
+            else {
+                l = mid + 1;
+            }
+        }
+        
+        return false;
+    }
+};
+
+
+// https://www.hrwhisper.me/leetcode-valid-perfect-square/
 class Solution {
 public:
     bool isPerfectSquare(int num) {
