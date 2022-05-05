f79521300f076f45c957c482d38f3e8e441926de
ndhuanhuan <ndhuanhuan@gmail.com>
2022-05-04 21:52:54 -0700

    Update 671. Second Minimum Node In a Binary Tree.cpp

--- 601-800/671. Second Minimum Node In a Binary Tree.cpp
+++ 601-800/671. Second Minimum Node In a Binary Tree.cpp
@@ -1,3 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findSecondMinimumValue(TreeNode* root) {
+        if(!root) return -1;
+        deque<TreeNode*> q;
+        q.push_back(root);
+        // Smallest value
+        int s1 = root->val;
+        // Sencond smallest value
+        int s2 = -1;
+        bool found = false;
+        while(!q.empty()) {
+            auto cur = q.front();
+            q.pop_front();
+            // Keep updating second smallest value
+            if(cur->val > s1 && (s2 == -1 || cur->val < s2)) {
+                s2 = cur->val;
+                found = true;
+                // No need to add it's children into queue
+                continue; 
+            }
+            
+            if(cur->left) q.push_back(cur->left);
+            if(cur->right) q.push_back(cur->right);
+        }
+        
+        return s2;
+    }
+};
+
 //http://www.cnblogs.com/grandyang/p/7590156.html
 class Solution {
 public:
