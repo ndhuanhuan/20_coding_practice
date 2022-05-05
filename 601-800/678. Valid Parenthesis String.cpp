f82236c569e1b4b45a9e31c072465dabf2306669
ndhuanhuan <ndhuanhuan@gmail.com>
2022-05-05 09:51:05 -0700

    Update 678. Valid Parenthesis String.cpp

--- 601-800/678. Valid Parenthesis String.cpp
+++ 601-800/678. Valid Parenthesis String.cpp
@@ -1,3 +1,29 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int cmin = 0, cmax = 0;
+        
+        for(char c: s) {
+            if(c == '(') {
+                cmin++;
+                cmax++;
+            } else if(c == ')') {
+                cmin--;
+                cmax--;
+            } else {
+                cmax++;
+                cmin--;
+            }
+            
+            if(cmax < 0) return false;
+            cmin = max(cmin, 0); // right parenthese cannot go below 0, we can assume all matching if it is below 0
+        }
+        
+        return cmin == 0;
+    }
+};
+
+
 // https://zxi.mytechroad.com/blog/dynamic-programming/leetcode-678-valid-parenthesis-string/
 class Solution {
 public:
