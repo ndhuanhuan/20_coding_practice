99e933ae732eca79e45bdfd5a1fce16a69ef7eac
ndhuanhuan <ndhuanhuan@gmail.com>
2022-05-16 18:47:04 -0700

    Update 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp

--- 1401-1600/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
+++ 1401-1600/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
@@ -1,3 +1,29 @@
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& A, int limit) {
+        deque<int> maxd, mind;
+        int res = 0;
+        int i = 0, j;
+        for (j = 0; j < A.size(); ++j) {
+            while (!maxd.empty() && A[j] > maxd.back()) maxd.pop_back();
+            while (!mind.empty() && A[j] < mind.back()) mind.pop_back();
+            maxd.push_back(A[j]);
+            mind.push_back(A[j]);
+            while (maxd.front() - mind.front() > limit) {
+                if (maxd.front() == A[i]) maxd.pop_front();
+                if (mind.front() == A[i]) mind.pop_front();
+                ++i;
+            }
+            
+            res = max(res, j - i + 1);
+        }
+        return res;
+    }
+};
+
+
+
 // https://zxi.mytechroad.com/blog/queue/leetcode-1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/
 class Solution {
 public:
