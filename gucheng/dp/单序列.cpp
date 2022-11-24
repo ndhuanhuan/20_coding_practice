3b975bc3d7628053082851981266968fa131c8e2
ndhuanhuan <ndhuanhuan@gmail.com>
2022-11-23 20:53:27 -0800

    Update 单序列.cpp

--- gucheng/dp/单序列.cpp
+++ gucheng/dp/单序列.cpp
@@ -1,1 +1,14 @@
-
+// 509. Fibonacci Number
+class Solution {
+public:
+    int fib(int N) {
+        if(N < 2) return N;
+        vector<int> dp(N+1, 0);
+        dp[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i-1] + dp[i - 2];
+        }
+        
+        return dp[N];
+    }
+};
