f2c08d29ab1f10d0c471b18ee8d08977460f4194
ndhuanhuan <ndhuanhuan@gmail.com>
2022-11-27 15:12:04 -0800

    Update 单序列.cpp

--- gucheng/dp/单序列.cpp
+++ gucheng/dp/单序列.cpp
@@ -226,3 +226,26 @@ public:
     return dp.size();
   }
 };
+
+// 1713. Minimum Operations to Make a Subsequence
+
+class Solution {
+public:
+  int minOperations(vector<int>& target, vector<int>& arr) {
+    unordered_map<int, int> m;
+    for (int i = 0; i < target.size(); ++i)
+      m[target[i]] = i;
+    vector<int> s;
+    for (int a : arr) {
+      auto mit = m.find(a);
+      if (mit == end(m)) continue;
+      const int idx = mit->second;
+      auto it = lower_bound(begin(s), end(s), idx);
+      if (it == end(s))
+        s.push_back(idx);
+      else
+        *it = idx;
+    }
+    return target.size() - s.size();
+  }
+};
