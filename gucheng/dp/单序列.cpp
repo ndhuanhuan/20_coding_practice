7daea0e0c08d5723600cad54e8089e0db68923e2
ndhuanhuan <ndhuanhuan@gmail.com>
2022-11-27 15:00:56 -0800

    Update 单序列.cpp

--- gucheng/dp/单序列.cpp
+++ gucheng/dp/单序列.cpp
@@ -210,3 +210,19 @@ public:
 };
 
 // 300. Longest Increasing Subsequence
+class Solution {
+public:  
+  int lengthOfLIS(vector<int>& nums) {
+    const int n = nums.size();
+    if (n == 0) return 0;    
+    vector<int> dp;
+    for (int i = 0; i < n; ++i) {
+      auto it = lower_bound(begin(dp), end(dp), nums[i]);
+      if (it == end(dp))
+        dp.push_back(nums[i]);
+      else
+        *it = nums[i];
+    }
+    return dp.size();
+  }
+};
