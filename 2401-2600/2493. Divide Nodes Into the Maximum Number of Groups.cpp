d5b695dd62ac460297dd5db3ac5c6ae01003148d
ndhuanhuan <ndhuanhuan@gmail.com>
2022-12-04 13:48:27 -0800

    Update 2493. Divide Nodes Into the Maximum Number of Groups.cpp

--- 2401-2600/2493. Divide Nodes Into the Maximum Number of Groups.cpp
+++ 2401-2600/2493. Divide Nodes Into the Maximum Number of Groups.cpp
@@ -1,1 +1,2 @@
 // https://leetcode.cn/problems/divide-nodes-into-the-maximum-number-of-groups/solution/mei-ju-qi-dian-pao-bfs-by-endlesscheng-s5bu/
+// https://www.bilibili.com/video/BV15d4y147YF/?vd_source=48e4bed01dd155111c1b309b768743f6
