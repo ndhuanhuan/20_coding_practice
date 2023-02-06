205ac1502298f180471f85ee80c1e09e413d624f
ndhuanhuan <ndhuanhuan@gmail.com>
2023-02-05 22:42:28 -0800

    Create 2556. Disconnect Path in a Binary Matrix by at Most One Flip.cpp

---
+++ 2401-2600/2556. Disconnect Path in a Binary Matrix by at Most One Flip.cpp
@@ -0,0 +1,16 @@
+
+// https://leetcode.cn/problems/disconnect-path-in-a-binary-matrix-by-at-most-one-flip/solution/zhuan-huan-cheng-qiu-lun-kuo-shi-fou-xia-io8x/
+class Solution {
+public:
+    bool isPossibleToCutPath(vector<vector<int>> &g) {
+        int m = g.size(), n = g[0].size();
+        function<bool(int, int)> dfs = [&](int x, int y) -> bool { // 返回能否到达终点
+            if (x == m - 1 && y == n - 1) return true;
+            g[x][y] = 0; // 直接修改
+            return x < m - 1 && g[x + 1][y] && dfs(x + 1, y) ||
+                   y < n - 1 && g[x][y + 1] && dfs(x, y + 1);
+        };
+        return !dfs(0, 0) || !dfs(0, 0);
+    }
+};
+
