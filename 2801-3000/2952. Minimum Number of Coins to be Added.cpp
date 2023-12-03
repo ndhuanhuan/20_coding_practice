7779d77c3c4a85824c8c3bedf00c450f5331b2b2
ndhuanhuan <ndhuanhuan@gmail.com>
2023-12-03 12:28:23 -0800

    Create 2952. Minimum Number of Coins to be Added.cpp

---
+++ 2801-3000/2952. Minimum Number of Coins to be Added.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumAddedCoins(vector<int> &coins, int target) {
+        sort(coins.begin(), coins.end());
+        int ans = 0, s = 1, i = 0;
+        while (s <= target) {
+            if (i < coins.size() && coins[i] <= s) {
+                s += coins[i];
+                i++;
+            } else {
+                s *= 2; // 必须添加 s
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
