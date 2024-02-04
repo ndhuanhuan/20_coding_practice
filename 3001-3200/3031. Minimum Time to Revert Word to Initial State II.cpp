43b957507f4108394af10a2b691141109033273d
ndhuanhuan <ndhuanhuan@gmail.com>
2024-02-04 11:55:43 -0800

    Create 3031. Minimum Time to Revert Word to Initial State II.cpp

---
+++ 3001-3200/3031. Minimum Time to Revert Word to Initial State II.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minimumTimeToInitialState(string s, int k) {
+        int n = s.size();
+        vector<int> z(n);
+        int l = 0, r = 0;
+        for (int i = 1; i < n; i++) {
+            if (i <= r) {
+                z[i] = min(z[i - l], r - i + 1);
+            }
+            while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+                l = i;
+                r = i + z[i];
+                z[i]++;
+            }
+            if (i % k == 0 && z[i] >= n - i) {
+                return i / k;
+            }
+        }
+        return (n - 1) / k + 1;
+    }
+};
+
+作者：灵茶山艾府
+链接：https://leetcode.cn/problems/minimum-time-to-revert-word-to-initial-state-ii/solutions/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
