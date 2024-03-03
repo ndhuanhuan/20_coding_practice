952be2eeeb0dc11bd68d670da0e95e245aa95b67
ndhuanhuan <ndhuanhuan@gmail.com>
2024-03-03 13:37:34 -0800

    Create 3072. Distribute Elements Into Two Arrays II.cpp

---
+++ 3001-3200/3072. Distribute Elements Into Two Arrays II.cpp
@@ -0,0 +1,56 @@
+// https://www.bilibili.com/video/BV14r421W7oR/?vd_source=48e4bed01dd155111c1b309b768743f6
+// https://leetcode.cn/problems/distribute-elements-into-two-arrays-ii/solutions/2664646/chi-san-hua-shu-zhuang-shu-zu-pythonjava-3bb2/
+class Fenwick {
+    vector<int> tree;
+
+public:
+    Fenwick(int n) : tree(n) {}
+
+    // 把下标为 i 的元素增加 1
+    void add(int i) {
+        while (i < tree.size()) {
+            tree[i] += 1;
+            i += i & -i;
+        }
+    }
+
+    // 返回下标在 [1,i] 的元素之和
+    int pre(int i) {
+        int res = 0;
+        while (i > 0) {
+            res += tree[i];
+            i &= i - 1;
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    vector<int> resultArray(vector<int> &nums) {
+        auto sorted = nums;
+        ranges::sort(sorted);
+        sorted.erase(unique(sorted.begin(), sorted.end()), sorted.end());
+        int m = sorted.size();
+
+        vector<int> a{nums[0]}, b{nums[1]};
+        Fenwick t1(m + 1), t2(m + 1);
+        t1.add(ranges::lower_bound(sorted, nums[0]) - sorted.begin() + 1);
+        t2.add(ranges::lower_bound(sorted, nums[1]) - sorted.begin() + 1);
+        for (int i = 2; i < nums.size(); i++) {
+            int x = nums[i];
+            int v = ranges::lower_bound(sorted, x) - sorted.begin() + 1;
+            int gc1 = a.size() - t1.pre(v); // greaterCount(a, v)
+            int gc2 = b.size() - t2.pre(v); // greaterCount(b, v)
+            if (gc1 > gc2 || gc1 == gc2 && a.size() <= b.size()) {
+                a.push_back(x);
+                t1.add(v);
+            } else {
+                b.push_back(x);
+                t2.add(v);
+            }
+        }
+        a.insert(a.end(), b.begin(), b.end());
+        return a;
+    }
+};
